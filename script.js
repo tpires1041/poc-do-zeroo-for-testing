fe43de7aa8780d5b0afc42d0140afeaa66aadf5a
joaopedropicolo <bigwon132@gmail.com>
2024-08-06 22:40:45 -0300

    Atualização
    Atualizado de acordo com o outro repositório.

--- script.js
+++ script.js
@@ -21,14 +21,47 @@ Object.entries(answers).forEach(([key, value]) => {
     await manager.train();
     manager.save();
 
-    const bott = new TelegramBot('7331736184:AAH5KO-X0DrTT0f3APAkgSR01bKo4HLhs_k', { polling: true });
+    const bott = new TelegramBot('6782553841:AAGIBnF0xW-z48isb9Lsq_cPp4m4-UFRuL4', { polling: true });
 
     bott.onText(/\/echo (.+)/, (msg, match) => bott.sendMessage(msg.chat.id, match[1]));
 
     bott.on('message', async (msg) => {
         const chatId = msg.chat.id;
         const response = await manager.process('pt', msg.text.toLowerCase());
-        const reply = response.answer || "Desculpe, não entendi sua mensagem. 😢\n\nPoderia repetir de uma forma diferente?";
-        bott.sendMessage(chatId, reply, { parse_mode: 'HTML' });
+        if(response.answer == null){
+
+            const errorMessages = [
+                "Desculpe, não entendi sua mensagem. 🤔",
+                "Parece que não consegui compreender o que você disse. 🤔",
+                "Essa mensagem está um pouco confusa para mim. 😕",
+                "Não consegui entender sua mensagem. Pode reformular? 🤔",
+                "Ops, não consegui captar sua mensagem. 😕",
+                "Hmm, não consegui entender o que você quis dizer. 😕",
+                "Desculpe, estou tendo dificuldades para entender sua mensagem. 🤔",
+                "Poderia explicar de outra forma? 🤔",
+                "Infelizmente, não consegui entender isso. 😕",
+                "Pode tentar perguntar de outra maneira? 🤔"
+            ];
+
+            const errorMessage = errorMessages[Math.floor(Math.random() * errorMessages.length)];
+            bott.sendMessage(chatId, errorMessage, { parse_mode: 'HTML' }); 
+            
+        } else {
+
+            const responseMessages = [
+                "Aqui está a resposta do que você pediu: 📝",
+                "Esta é a informação que você solicitou: 📚",
+                "Encontrei a resposta para você: 🔍",
+                "Aqui está o que você queria saber: 💡",
+                "Sua resposta é a seguinte: ✅",
+                "Aqui está o que você está procurando: 😀",
+                "A resposta é a seguinte: 😉"
+            ];
+
+            const responseMessage = responseMessages[Math.floor(Math.random() * responseMessages.length)];
+            bott.sendMessage(chatId, responseMessage, { parse_mode: 'HTML' });
+            const reply = response.answer;
+            await bott.sendMessage(chatId, reply, { parse_mode: 'HTML' });
+        }
     });
 })();
